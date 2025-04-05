97503a38aadbffb994dc6fc377a748c6e1ad8bf1
joaquin <jpareja@mergeworld.com>
2025-04-04 22:58:15 -0300

    latest changes

--- BackEnd/readJPGAndConvertItIntoJsonFile.tsx
+++ BackEnd/readJPGAndConvertItIntoJsonFile.tsx
@@ -13,38 +13,43 @@ const currentTome= "StormcastEternalsTome-"
 import {ensureDirectoryExistence} from "../Utils/CheckFolderExists.js"
 const   convertImageToText =async(currentNumber: string)=> {
   const worker = await createWorker('eng');
-  const ret = await worker.recognize("./public/Images/"+currentTome+currentNumber+".jpg");
+  const ret = await worker.recognize("./public/Images/"+currentTome+currentNumber+".jpg");//https://lenguajejs.com/javascript/modulos/dynamic-import/
   await worker.terminate();
   return ret.data.text   
 }
 
 const convertToJson =async ()=>{//outputJson
-  const firstNumberOfUnits=8;
-  const lastNumberOfUnits=60;
+  const firstNumberOfUnits=10;
+  const lastNumberOfUnits=70;
   let index:string|number;
 
   checkAndAssingImage.forEach(async (str)=>{
     const eachTextOfEachBattleTomeCard:string=str!//https://stackoverflow.com/questions/45194964/how-to-assign-string-undefined-to-string-in-typescript
-  for(let i= firstNumberOfUnits; i<=lastNumberOfUnits; i++){
-    if(i==8||9){
-      index="0"+i;
-      if(i> 9){
-        index=0;
-        index=i;
-      }
+  for(var i= firstNumberOfUnits; i<=lastNumberOfUnits;i++){
+    index=i;
+    if(i==8||i==9){
+    index="0"+i;
+    }
+    if(i> 10){
+      index=0;
+      index=i;
     }
     const currentImageTextFromConvertedPDF= await convertImageToText(index.toString());
-    const fileName=`${currentTome}${index}.json`;
     if(currentImageTextFromConvertedPDF.includes(eachTextOfEachBattleTomeCard)){
-       console.log(eachTextOfEachBattleTomeCard)
+      index =Number(index) -1; // Assign it -1 as the number comes up always +1
+      const fileName=`Json/${currentTome}${(index)}.json`;
+      console.log(eachTextOfEachBattleTomeCard)
+      console.log(fileName);
+      console.log(index)
         const jsonData = {
         filename: `${currentTome}${index}`,
         extracted_text: eachTextOfEachBattleTomeCard,
           };
-          ensureDirectoryExistence("Json/"+fileName)
-        fs.writeFileSync("Json/"+fileName, JSON.stringify(jsonData, null,2));
+          ensureDirectoryExistence(fileName)
+        fs.writeFileSync(fileName, JSON.stringify(jsonData, null,2));
         } 
   }
+  
 })
   
 }
