55479a9b52eeb393a64b42c4ae208628e42fd57d
joaquin <jpareja@mergeworld.com>
2025-04-09 15:15:13 -0300

    latest changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
-// /**
-//  *  @type {import {'next'}.NextConfig}
-//  */
-const isProd = process.env.NODE_ENV === 'production';
+/**
+ *  @type {import {'next'}.NextConfig}
+ */
+// const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/../JoaqPareja/TestTomes/' : '',
-  basePath: isProd ? '' : '',
-  output: 'export'
+  // assetPrefix: isProd ? '/../JoaqPareja/TestTomes/' : '',
+  // basePath: isProd ? '' : '',
+  // output: 'export'
 };
 
 
