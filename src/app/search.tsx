3fba243ab404dd3890d611670d865e73034e0428
joaquin <jpareja@mergeworld.com>
2025-04-10 09:46:29 -0300

    latest changes

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -7,26 +7,91 @@ import React, { useState } from 'react';
 // })
 import StormcastEternalsTomeFile from "../../Json/StormcastEternalsTome.json"
 import './styles.css';
+const possibleSearches=["LIBERATORS", "KNIGHT-ARCANUM", "YNDRASTA", "ANNIHILATORS", "WITH METEORIC GRANDHAMMERS", "PROSECUTORS", "STARSTRIKE JAVELINS", "SHOCK HANDAXES", "VIGILORS", "CELESTIAN VORTEX", "DAIS ARCANUM", "GRYPH-STALKER", "LORD-TERMINOS", "RECLUSIANS"]
+let text: string;
+let valueFile:string;
+//   const sayHello = () => {
+//     // const [valueName, setLookOutForCard] = useState('')
+//     // setLookOutForCard(valueName)
+//     // alert("Hello!")
 
+//     // const handleClick = (e: { preventDefault: () => void; }) => {
+//     //   e.preventDefault();
+//       alert("Hello!")
+
+//     // }
+//     // return handleClick
+//   };
+
+
+//     const [open, setOpen] = useState(null);
+
+//     const [showImage, setShowImage] = useState({
+//       showImageNow: true,
+//       showImageId: null,
+//     })
+
+//     const {showImageNow, showImageId} = showImage;
+
+//     const OpenImage = (a: { image: React.SetStateAction<null>; id: any; }) => {
+//      setOpen(a.image);
+//      setShowImage({
+//        showImageNow: !showImageNow,
+//        showImageId: a.id
+//      }); 
+//     }
+
+const ImgIteration=(valueName:string)=>{
+  {
+    StormcastEternalsTomeFile.map((eachCard) => {
+      if(valueName===eachCard.extracted_text.slice(0,1)+eachCard.extracted_text.slice(1).toLowerCase()){
+        text= eachCard.extracted_text.slice(0,1)+eachCard.extracted_text.slice(1).toLowerCase();
+        valueFile = eachCard.filename;
+               }
+            })
+          }
+}
+
+const ImageToBeSearched=(valueName: React.Key | null | undefined)=>{
+setTimeout(()=>{
+return(
+  <div key={valueName}>
+  <img 
+    src={"./Images/"+valueFile+".jpg"} 
+    alt={valueFile}
+    id='imgTest'
+    /> 
+</div>
+)
+},1000)
+}
 const SearchesMap =()=>{
-  const possibleSearches=["LIBERATORS", "KNIGHT-ARCANUM", "YNDRASTA", "ANNIHILATORS", "WITH METEORIC GRANDHAMMERS", "PROSECUTORS", "STARSTRIKE JAVELINS", "SHOCK HANDAXES", "VIGILORS", "CELESTIAN VORTEX", "DAIS ARCANUM", "GRYPH-STALKER", "LORD-TERMINOS", "RECLUSIANS"]
+
   let possibleSearchsPArr=[]
   for (let i = 0; i < possibleSearches.length; i++) {
     possibleSearchsPArr.push(
-    <p className="testText" key={possibleSearches[i]}>
+    <p className="testText" key={possibleSearches[i]} 
+    // onClick={() => OpenImage(a)}
+    >
         {possibleSearches[i].slice(0,1)+possibleSearches[i].slice(1).toLowerCase()},</p>)
   }
   return(
-
+    <>
   <div className="allingText" >
     {possibleSearchsPArr.map(p=>p)}
     </div>
+     {/* {
+      showImageNow && showImageId === a.id ?
+        <>
+        {ImageToBeSearched(valueName)}
+      </>                  
+    } */}
+  </>
     )
   
   }
 
-let text: string;
-let valueFile:string;
+
         export default  function Search() {
           const [valueName, setLookOutForCard] = useState('');
           return (
@@ -51,28 +116,11 @@ let valueFile:string;
                <hr />
 
               </div>
-              {
-              StormcastEternalsTomeFile.map((eachCard) => {
-                if(valueName===eachCard.extracted_text.slice(0,1)+eachCard.extracted_text.slice(1).toLowerCase()){
-                  text= eachCard.extracted_text.slice(0,1)+eachCard.extracted_text.slice(1).toLowerCase();
-                  valueFile = eachCard.filename;
-                         }
-                      })
-                    }
+              {ImgIteration(valueName)}
               {
                 valueName==text&&
                   <>
-                    <div key={valueName}>
-                    <img 
-                      src={"./Images/"+valueFile+".jpg"} 
-                      alt={valueFile}
-                      // sizes="100vw"  
-                      style={{
-                          width: 'auto',  
-                          height: 'auto',
-                        }}
-                      /> 
-                  </div>
+                  {ImageToBeSearched(valueName)}
                 </>                  
             }
                  
