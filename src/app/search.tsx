c1594e4d78720a1e1485b9bf6825f40e06aa165d
joaquin <jpareja@mergeworld.com>
2025-04-10 17:20:59 -0300

    latest changes

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 // const filename = "StormcastEternalsTome-";
 
 // const testFiles = (currentNumberFile:number|string)=>import (`../../Json/${filename}${currentNumberFile}.json`).then((str)=>{
@@ -8,44 +8,16 @@ import React, { useState } from 'react';
 import StormcastEternalsTomeFile from "../../Json/StormcastEternalsTome.json"
 import './styles.css';
 const possibleSearches=["LIBERATORS", "KNIGHT-ARCANUM", "YNDRASTA", "ANNIHILATORS", "WITH METEORIC GRANDHAMMERS", "PROSECUTORS", "STARSTRIKE JAVELINS", "SHOCK HANDAXES", "VIGILORS", "CELESTIAN VORTEX", "DAIS ARCANUM", "GRYPH-STALKER", "LORD-TERMINOS", "RECLUSIANS"]
-let text: string;
+let textFromImgIteration: string;
 let valueFile:string;
-//   const sayHello = () => {
-//     // const [valueName, setLookOutForCard] = useState('')
-//     // setLookOutForCard(valueName)
-//     // alert("Hello!")
-
-//     // const handleClick = (e: { preventDefault: () => void; }) => {
-//     //   e.preventDefault();
-//       alert("Hello!")
-
-//     // }
-//     // return handleClick
-//   };
 
 
-//     const [open, setOpen] = useState(null);
-
-//     const [showImage, setShowImage] = useState({
-//       showImageNow: true,
-//       showImageId: null,
-//     })
-
-//     const {showImageNow, showImageId} = showImage;
-
-//     const OpenImage = (a: { image: React.SetStateAction<null>; id: any; }) => {
-//      setOpen(a.image);
-//      setShowImage({
-//        showImageNow: !showImageNow,
-//        showImageId: a.id
-//      }); 
-//     }
 
 const ImgIteration=(valueName:string)=>{
   {
     StormcastEternalsTomeFile.map((eachCard) => {
       if(valueName===eachCard.extracted_text.slice(0,1)+eachCard.extracted_text.slice(1).toLowerCase()){
-        text= eachCard.extracted_text.slice(0,1)+eachCard.extracted_text.slice(1).toLowerCase();
+        textFromImgIteration= eachCard.extracted_text.slice(0,1)+eachCard.extracted_text.slice(1).toLowerCase();
         valueFile = eachCard.filename;
                }
             })
@@ -64,33 +36,65 @@ return(
 </div>
 )
 }
-const SearchesMap =()=>{
 
-  let possibleSearchsPArr=[]
-  for (let i = 0; i < possibleSearches.length; i++) {
-    possibleSearchsPArr.push(
-    <p className="testText" key={possibleSearches[i]} 
-    // onClick={() => OpenImage(a)}
-    >
-        {possibleSearches[i].slice(0,1)+possibleSearches[i].slice(1).toLowerCase()},</p>)
-  }
+
+const SearchesMap =()=>{
+//   const [name, setName] = useState('');
+//   // const [isTextClicked, setTextBool] = useState(false);
+
+//   const changeName = () => {
+//     let value = name;
+
+//     if (value === valueFile) {
+//       setName(valueFile);
+
+//     } else {
+//       setName('');
+//     }
+//     // {/* {ImgIteration('')}   */}
+//   }
+  // onClick={changeName}
+
   return(
     <>
-  <div className="allingText" >
-    {possibleSearchsPArr.map(p=>p)}
-    </div>
-     {/* {
-      showImageNow && showImageId === a.id ?
-        <>
-        {ImageToBeSearched(valueName)}
-      </>                  
-    } */}
+    <section> 
+  <div className="allingText" key={'allingText'} >
+    {possibleSearches.map(index=>{
+      process.env.currentTest=index.slice(0,1)+index.slice(1).toLowerCase()
+  // console.log(index.slice(0,1)+index.slice(1).toLowerCase())
+  return(
+    <>
+
+
+    <p  className="testText"  key={index}>
+   {index.slice(0,1)+index.slice(1).toLowerCase()}</p>
+    {/*
+    {/* <button value={index} key={index}  
+ onClick={(e)=>{
+//   process.env.Value=e.currentTarget.value
+//   alert(e.currentTarget.value)}}
+    </button> */}
+
   </>
+  )
+})
+
+}
+    </div>
+  </section>
+ <>
+ { console.log(process.env.Value)}
+ {/* <img src={"./Images/"+"StormcastEternalsTome-29"+".jpg"} alt='test' style={{width:'100%',height:'100%'}}>
+ </img> */}
+ {/* {ImgIteration(`${process.env.Value}`)}
+    {
+      process.env.Value==textFromImgIteration&&
+        ImageToBeSearched(`${process.env.Value}`)
+      } */}
+ </>
+    </>
     )
-  
   }
-
-
         export default  function Search() {
           const [valueName, setLookOutForCard] = useState('');
           return (
@@ -103,7 +107,7 @@ const SearchesMap =()=>{
               <label>
                 Please search for the desired card:
                 <input
-                  value={valueName}
+                  value={process.env.Value =valueName}
                   onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setLookOutForCard(e.target.value)}
                 />
               </label>
@@ -112,17 +116,17 @@ const SearchesMap =()=>{
               <p>Posibble searches: </p>
               <hr />
                <SearchesMap/>
-               <hr />
 
               </div>
-              {ImgIteration(valueName)}
+              {ImgIteration(process.env.Value)}
               {
-                valueName==text&&
+                process.env.Value==textFromImgIteration&&
                   <>
-                  {ImageToBeSearched(valueName)}
+                  {ImageToBeSearched(process.env.Value)}
+
                 </>                  
             }
-                 
+
           </>
           );
         }
